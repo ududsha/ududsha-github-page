b85303a37098581ec693a92255d34f316f1c4e88
udayahaulmatic <udaya@haulmatic.com>
2017-07-27 01:03:46 +0530

    Update the pages to work with my site

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ function parseRedirectQuery(query, replace) {
 // a redirect to the domain with no path is required.
 // https://help.github.com/articles/custom-domain-redirects-for-github-pages-sites/
 // SET THIS: e.g. my-repo-name
-const gitHubRepoName = 'react-github-pages';
+const gitHubRepoName = 'ududsha-github-page';
 // The domain for your site
 // SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
-const domain = 'http://react-github-pages.rafrex.com';
+const domain = 'http://ududsha.github.io';
 function redirectToDomain() {
   window.location.replace(domain);
 }
